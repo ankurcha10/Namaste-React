9cc4a4531d008715416120508cb305fb2b0857e8
unknown <ankurcha10@gmail.com>
2023-08-24 13:29:13 +0530

    basic structure

---
+++ App.js
@@ -0,0 +1,23 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am h1 tag")
+  )
+);
+
+const heading = [React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello World From React!"
+  ), React.createElement(
+    "h2",
+    { id: "heading" },
+    "Hello World From React!"
+  )];
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
